5a0f0cd05dfab891d94d0b667e8e5e247457c028
czechbol <adamludes@gmail.com>
2023-04-22 16:00:52 +0200

    reworked new_project.py

--- new_project.py
+++ new_project.py
@@ -1,30 +1,40 @@
-import sys
+#! /usr/bin/env python
+
+import argparse
 from pathlib import Path
 from shutil import copytree, ignore_patterns
 
 
-# This script initializes new pytorch project with the template files.
-# Run `python3 new_project.py ../MyNewProject` then new project named
-# MyNewProject will be made
-current_dir = Path()
-assert (
-    current_dir / "new_project.py"
-).is_file(), "Script should be executed in the pytorch-template directory"
-assert (
-    len(sys.argv) == 2
-), "Specify a name for the new project. Example: python3 new_project.py MyNewProject"
+def new_project(script_parent: Path, target_dir: Path):
+    """Initialize new pytorch project with the template files.
 
-project_name = Path(sys.argv[1])
-target_dir = current_dir / project_name
+    Args:
+        target_dir (Path): Target directory for the new project.
+    """
+    ignore = [
+        ".git",
+        "data",
+        "saved",
+        ".venv",
+        "venv",
+        "new_project.py",
+        "__pycache__",
+    ]
+    copytree(script_parent, target_dir, ignore=ignore_patterns(*ignore))
+    print("New project initialized at", target_dir.absolute().resolve())
 
-ignore = [
-    ".git",
-    "data",
-    "saved",
-    "new_project.py",
-    "LICENSE",
-    "README.md",
-    "__pycache__",
-]
-copytree(current_dir, target_dir, ignore=ignore_patterns(*ignore))
-print("New project initialized at", target_dir.absolute().resolve())
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="PyTorch Template")
+    args.add_argument(
+        "-d",
+        "--destination",
+        default="configs/mnist.json",
+        type=str,
+        help="Destination path for the new project",
+        required=True,
+    )
+    parsed_args = args.parse_args()
+    script_parent = Path(__file__).resolve().parent
+
+    new_project(script_parent, Path(parsed_args.destination))
